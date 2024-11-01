41e2b8f92dbda85b3a835a5471bf55f8437a75b9
mapacheh <mairapacheco.002@gmail.com>
2024-11-01 07:18:14 -0500

    Update ejercicio02.R

--- ejercicio02.R
+++ ejercicio02.R
@@ -2,8 +2,8 @@
 
 solucion <- function() {
   # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
-  a <- 200 # asigna valor aquí
-  b <- 100 # asigna valor aquí
+  a <- 150 # asigna valor aquí
+  b <- 50 # asigna valor aquí
   s <- a-b
   return(s)
 }
